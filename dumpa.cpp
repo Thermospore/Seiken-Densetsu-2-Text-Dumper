7e8cc2692b82d131389eed1466d627eb6b121aea
Thermospore <thermospore.main@gmail.com>
2020-06-09 23:58:01 -0800

    move RET to sandwich formation

--- dumpa.cpp
+++ dumpa.cpp
@@ -4,13 +4,13 @@
 //goal: match appearance of in game textbox (bar debug output)
 //check rest of stuff in badParses
 //fix string coding stuff, so it doesn't take forever to compile...
-//in dump, don't base box spacing on <pause>. maybe count rows->3 or something?
-//		make a function specifically for handling in game NLs, and have it keep track of rows?
-//		and any debug NLs should be purely on top of that
-//		gNL; CWRAP and RWRAP?
 //option to A) show exactly like game or B) show special codes too (debug output)
-//		easiest to do in post processing, rather than adding an if to every single debug output?
 //		maybe do it compacktly with that ? operator thing
+//		maybe easiest to do in post processing, rather than adding an if to every single debug output?
+//			separate script to strip out all other <*>s (besides the above)
+//			remove any consecutive NLs
+//		maybe only show "sandwich"ing codes like open, close, and ret?
+//		maybe a separate script to strip out all other <*>s (besides the above)
 
 //advances to next byte
 void advance(unsigned char &cur, FILE* pROM, int &addr)
@@ -134,7 +134,7 @@ int main()
 			advance(cur,pROM,addr);
 			if(cur == 0xFF)
 			{
-				fprintf(pDUMP,"<RET>",cur);
+				fprintf(pDUMP,"\n<RET>",cur);
 			}
 			else
 			{
